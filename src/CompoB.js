edca78e20cae3525f1e972452a4e4476d1a886eb
kavita01sharma <sharma87kavita@gmail.com>
2021-04-07 17:03:51 +0530

    usecontext

--- src/CompoB.js
+++ src/CompoB.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import CompoC from './CompoC';
+import {usercontext, userchannel} from './App'
 
 const CompoB=()=>{
+    let user = useContext(usercontext);
+    let channel = useContext(userchannel);
     return (
         <>
-        <CompoC/>
+            <h2>My name is {user} and my channel is {channel}</h2>
         </>
     )
 }
